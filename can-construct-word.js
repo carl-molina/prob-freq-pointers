6e2cc0bf324ff41c2f72374631e7f756b7a40300
Carl Molina <carlandrew.molina@gmail.com>
2023-11-10 15:35:00 -0800

    can-construct-word passes tests

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,34 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+
+
+/**
+ * Takes two strings; checks to see if the first string can be built from
+ * second string; returns boolean.
+ */
+
+function canConstructWord(word, letters) {
+  const wordFreq = getFrequencyCounter(word);
+  const lettersFreq = getFrequencyCounter(letters);
+
+  for (let key in wordFreq) {
+    if (lettersFreq[key] === undefined) {
+      return false;
+    } else if (wordFreq[key] > lettersFreq[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+function getFrequencyCounter(str) {
+  const freqs = {};
+
+  for (const char of str) {
+    const curr = freqs[char] || 0;
+    freqs[char] = curr + 1;
+  }
+
+  return freqs;
 }
