42389a7e9049c339d7cbc493a1cafee8a60146cf
Carl Molina <carlandrew.molina@gmail.com>
2023-11-10 23:23:06 -0800

    Added is-subsequence and count-pairs solution (<--still working on this one)

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,21 @@
-// add whatever parameters you deem necessary & write docstring
-function countPairs() {
+"use strict";
+
+
+
+/**
+ * Takes an array of integers and a number; returns the number of pairs of
+ * integers in the array whose sum is equal to the second parameter (number)
+ */
+
+function countPairs(integers, num) {
+  let count = 0;
+
+  for (let i = 0; i < integers.length - 1; i++) {
+    for (let j = i + 1; j <= integers.length; j++) {
+      if (integers[i] + integers[j] === num) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,36 @@
-// add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+"use strict";
+
+
+/**
+ * Takes two strings, s1 and s2; checks whether the chars in s1 `form a
+ * subsequence of the characters in `s2; returns boolean
+ *        ('sing', 'sting') // true
+ *        ('abc', 'acb') // false (order matters)
+ */
+
+function isSubsequence(s1, s2) {
+  let s1Idx = 0;
+  let s2Idx = 0;
+
+  while (s1Idx < s1.length) {
+    if (s1[s1Idx] === s2[s2Idx]) {
+      s1Idx++;
+      s2Idx++;
+    } else if (s1[s1Idx] !== s2[s2Idx]) {
+      s2Idx++;
+    }
+
+    // checks to see if subsequence is successful based on length
+    // example: if s1Idx = 4 and 'sing' length is 4, it's a subsequence
+    if (s1Idx === s1.length) {
+      return true;
+    }
+
+    // checks to see if there are no more chars left on s2
+    if (s2[s2Idx] === undefined) {
+      return false;
+    }
+
+  }
+  return false;
 }
