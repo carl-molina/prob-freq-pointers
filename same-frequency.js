2c05d9123acbe0605cf075d8d5575d1525045ed2
Seth Hobson <sethlawrencehobson@gmail.com>
2023-11-10 19:32:50 -0500

    finished same-frequency and two-array-object

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,38 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strint";
+
+/** takes two positive integers, determines if individual ints occur at same freq
+ * returns boolean *
+ */
+
+//take each integer turn to array
+// //init object on cb for get frequencycounter
+//loop through array, push each key value pair to obj
+//loop through object
+//compare values
+//if they don't have same values return false
+//else return true
+function sameFrequency(num1,num2) {
+  let firstInt = getFrequencyCounter(num1.toString());
+  let secondInt = getFrequencyCounter(num2.toString());
+
+  for (let number in firstInt) {
+    // debugger;
+    if (firstInt[number] !== secondInt[number]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+function getFrequencyCounter(str) {
+  const freqs = {};
+
+  for (const char of str) {
+    const curr = freqs[char] || 0;
+    freqs[char] = curr + 1;
+  }
+
+  return freqs;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,26 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** take two arrays of different lengths; return an object with the first array
+ * as keys and the second as values, keys w/o values will be null, extra values
+ * will be ignored.
+ */
+// init an obj
+//loop through the keys array (regular for loop)
+// loop for length of keys
+////if index > values length -1, obj[keys at index] should equal null
+//make the obj[keys at index 0] = [values at index 0]
+
+function twoArrayObject(keys, values) {
+  let arrayPairs = {};
+  let char;
+  // debugger;
+  for (let i = 0; i< keys.length; i++) {
+    char = keys[i]
+    if (i > values.length-1) {
+      arrayPairs[char] = null;
+    } else {
+      arrayPairs[char] = values[i];
+    }
+  }
+  return arrayPairs;
 }
