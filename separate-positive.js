bc175fb1293bbf67dcc2c0ae8db84330e03f00de
Carl Molina <carlandrew.molina@gmail.com>
2023-11-10 17:00:35 -0800

    End of Day

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,28 @@
-// add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+"use strict";
+
+
+/**
+ * Takes an array of non-zero numbers; separate positive numbers
+ * to the left and negative numbers to right of arr; returns
+ * original array
+ */
+
+function separatePositive(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (arr[left] > 0) {
+      left++;
+    }
+    if (arr[right] < 0) {
+      right--;
+    }
+    if (arr[left] < 0 && arr[right] > 0) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return arr;
 }
